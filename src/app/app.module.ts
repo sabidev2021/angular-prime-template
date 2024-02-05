b71a4c22529111d6682e2758df42d3218b82c740
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2024-02-06 00:09:01 +0700

    feat: implement test simple multi language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,15 @@ import { NotfoundComponent } from './shared/notfound/notfound.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/module/dashboard/dashboard-list/dashboard-list.component.ts
+++ src/app/module/dashboard/dashboard-list/dashboard-list.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-dashboard-list',
   templateUrl: './dashboard-list.component.html',
   styleUrls: ['./dashboard-list.component.scss']
 })
-export class DashboardListComponent  {
-  title = 'my-app';
+export class DashboardListComponent implements OnInit {
+
+  title = 'angular-prime-template';
+
+  constructor(private translateService: TranslateService) {}
+
+  ngOnInit(): void {
+    this.initLang()
+  }
+
+  initLang() {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translateService.setDefaultLang('in_ID');
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    this.translateService.use('in_ID');
+  }
+
 }
