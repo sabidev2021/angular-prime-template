9d7927455d343fc7dc35c76f24cf5eb12f5b0c68
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2024-02-06 06:10:15 +0700

    refactor: translate services

---
+++ src/app/lang/lang.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LangService {
+
+  constructor(private translateService: TranslateService) { }
+
+  setLanguage() {
+    this.translateService.addLangs(['in_ID', 'en_US']);
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translateService.setDefaultLang('in_ID');
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    this.translateService.use('in_ID');
+  }
+}
--- src/app/module/dashboard/dashboard-list/dashboard-list.component.ts
+++ src/app/module/dashboard/dashboard-list/dashboard-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LangService } from 'src/app/lang/lang.service';
 @Component({
   selector: 'app-dashboard-list',
   templateUrl: './dashboard-list.component.html',
@@ -9,17 +10,16 @@ export class DashboardListComponent implements OnInit {
 
   title = 'angular-prime-template';
 
-  constructor(private translateService: TranslateService) {}
+  constructor(
+    private langService: LangService
+  ) {}
 
   ngOnInit(): void {
     this.initLang()
   }
 
   initLang() {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    this.translateService.setDefaultLang('in_ID');
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    this.translateService.use('in_ID');
+    this.langService.setLanguage()
   }
 
 }
