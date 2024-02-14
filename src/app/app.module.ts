5cdc5488b022903063d75175e38ab4db58fa7078
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2024-02-15 05:39:04 +0700

    feat: sample configuration loader lang separately for child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,11 @@
-import { APP_INITIALIZER, Injector, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import {
-  IModuleTranslationOptions,
-  ModuleTranslateLoader,
-} from '@larscom/ngx-translate-module-loader';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-
-export function ModuleHttpLoaderFactory(http: HttpClient) {
-  const baseTranslateUrl = './assets/i18n';
-
-  const options: IModuleTranslationOptions = {
-    // version: Date.now(), // enable versioning
-    translateError: (error: any, path: any) => {
-      console.log('Oeps! an error occurred: ', { error, path });
-    },
-    modules: [
-      // final url: ./assets/i18n/en_US.json // currently
-      { baseTranslateUrl },
-      // final url: ./assets/i18n/sample-one/en_US.json
-      // { baseTranslateUrl, moduleName: 'sample-one' },
-      // final url: ./assets/i18n/sample-two/en_US.json
-      // { baseTranslateUrl, moduleName: 'sample-two' },
-    ],
-  };
-  return new ModuleTranslateLoader(http, options);
-}
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,15 +15,11 @@ export function ModuleHttpLoaderFactory(http: HttpClient) {
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: ModuleHttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
+    TranslateModule.forRoot({})
   ],
-  providers: [],
+  providers: [
+    TranslateService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
